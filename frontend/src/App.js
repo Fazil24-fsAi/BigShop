83ec8a3cfceb7a751f232f306dd3ca6cfa5ecd42
cipher <shaikhfazil542@gmail.com>
2022-09-07 20:40:26 +0530

    BigShop

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'> FsAi </a>
+      </header>
+      
+      <main> List Product </main>
+    </div>
+  );
+}
+
+export default App;
