b7313458e0231d3212e6c4b3921cb12e770baa7b
cipher <shaikhfazil542@gmail.com>
2022-09-07 23:07:38 +0530

    step2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import Data from "./Data";
 
 
 function App() {
@@ -7,7 +8,40 @@ function App() {
         <a href='/'> FsAi </a>
       </header>
       
-      <main> List Product </main>
+      <main> 
+        <h1>Featured Product</h1>
+
+        <div className="products">
+        {
+          Data.products.map((product =>(
+          <div  classname="product" key={product.slug}>
+
+           
+
+            <div className="hello">
+            <a href={`/product/${product.slug}`}>
+            <img src={product.images} alt={product.name} />
+               </a>
+
+            <p className="pp">
+           <a href={`/product/${product.slug}`}>
+             {product.name}
+              </a>
+
+              </p>
+              <p className="pp">
+              <strong>₹ {product.price}  </strong>
+                <p>
+                <button>Add to cart</button>
+                 </p>
+              </p>
+              </div>
+
+          </div>
+          )))}
+          </div>
+        
+         </main>
     </div>
   );
 }
---
+++ frontend/src/Data.js
@@ -0,0 +1,96 @@
+
+
+const Data = {
+products:[
+
+    {
+        name: 'Nike slim shirt',
+        slug:'Nike-slim-shirt',
+        category:'shirt',
+        images:'/images/female_img_1.jpg',
+        price:120,
+        countInStock:10,
+        brand:'Nike',
+        rating:4.5,
+        numreview: 10,
+        description:'high quality product',
+    },
+
+
+    {
+        name: 'Adidas fit shirt',
+        slug:'Adidas-slim-shirt',
+        category:'shirt',
+        images:'/images/female_img_4.jpg',
+        price:250,
+        countInStock:20,
+        brand:'Adidas',
+        rating:4.0,
+        numreview: 10,
+        description:'high quality product',
+    },
+
+
+    {
+        name: 'Nike slim pant',
+        slug:'Nike-slim-pant',
+        category:'pant',
+        images:'/images/male_img_1.jpg',
+        price:380,
+        countInStock:12,
+        brand:'Nike',
+        rating:4.5,
+        numreview: 14,
+        description:'high quality product',
+    },
+
+
+    {
+        name: 'Adidas slim pant',
+        slug:'Adidas -slim-pant',
+        category:'pant',
+        images:'/images/male_img_4.jpg',
+        price:55,
+        countInStock:2,
+        brand:'Adidas',
+        rating:4.5,
+        numreview: 10,
+        description:'high quality product',
+    },
+
+
+    {
+        name: 'Mr.FsAi',
+        slug:'Anytime Available',
+        category:'Heaven',
+        images:'/images/fazil.jpeg',
+        price:5000000000,
+        countInStock:1,
+        brand:'Ai',
+        rating:5,
+        numreview: 10,
+        description:'undefined',
+    },
+
+
+],
+};
+
+
+export default Data;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
